fb5db0a3cd3a5833e9c7661da85b93bd83ab5e41
zioIgna <ignaziocarbonaro@hotmail.com>
2018-06-24 23:33:45 +0200

    3.40 Create le API e stabilita connessione con Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,23 +1,34 @@
 import { Post } from './post.model';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
     private posts: Post[] = [];
-    private postsUpdate = new Subject<Post[]>();
+    private postsUpdated = new Subject<Post[]>();
+
+    constructor(private http: HttpClient) { }
 
     getPosts() {
-        return [...this.posts];
+        this.http.get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts')
+            .subscribe((postData) => {
+                this.posts = postData.posts;
+                this.postsUpdated.next([...this.posts]);
+            });
     }
 
     getPostUpdateListener() {
-        return this.postsUpdate.asObservable();
+        return this.postsUpdated.asObservable();
     }
 
     addPost(title: string, content: string) {
-        const post: Post = { title: title, content: content };
-        this.posts.push(post);
-        this.postsUpdate.next([...this.posts]);
+        const post: Post = { id: null, title: title, content: content };
+        this.http.post<{ message: string }>('http://localhost:3000/api/posts', post)
+            .subscribe(responseData => {
+                console.log(responseData.message);
+                this.posts.push(post);
+                this.postsUpdated.next([...this.posts]);
+            });
     }
 }

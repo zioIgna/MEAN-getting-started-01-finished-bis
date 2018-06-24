b3ebb7798c117331d4a8110f4e6f3366177d1385
zioIgna <ignaziocarbonaro@hotmail.com>
2018-06-24 12:21:54 +0200

    2.24 Adding Forms

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
-import { Post } from './../post.model';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Post } from './../post.model';
 
 @Component({
     selector: 'app-post-create',
@@ -11,8 +12,14 @@ export class PostCreateComponent {
     enteredContent = '';
     @Output() postCreated = new EventEmitter<Post>();
 
-    onAddPost() {
-        const post: Post = {title: this.enteredTitle, content: this.enteredContent};
+    onAddPost(form: NgForm) {
+        if (form.invalid) {
+            return;
+        }
+        const post: Post = {
+            title: form.value.title,
+            content: form.value.content,
+        };
         this.postCreated.emit(post);
     }
 }

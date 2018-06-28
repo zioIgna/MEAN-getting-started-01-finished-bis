6f7d807bc9865ee9f0cd2934ddf022d9c02a7248
zioIgna <ignaziocarbonaro@hotmail.com>
2018-06-28 11:24:25 +0200

    5.66 Prima di tagliare primo pezzo di indirizzo (/api/posts) (non funz)

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
-const Post = require('./models/post');
+const postRoutes = require("./routes/posts");
 
 const app = express();
 
@@ -31,69 +30,7 @@ app.use((req, res, next) => {
     next();
 });
 // Lm6DKaGZbpGfe58X
-app.post("/api/posts", (req, res, next) => {
-    const post = new Post({
-        _id: req.body.id,
-        title: req.body.title,
-        content: req.body.content
-    });
-    post.save().then(createdPost => {
-        res.status(201).json({
-            message: 'Post added successfully',
-            postId: createdPost._id
-        });
-    });
-});
 
-app.put("/api/posts/:id", (req, res, next) => {
-    const post = new Post({
-        _id: req.body.id,
-        title: req.body.title,
-        content: req.body.content
-    });
-    Post.updateOne({ _id: req.params.id }, post).then(result => {
-        // console.log(result);
-        res.status(200).json({ message: 'Update successful!' });
-    });
-});
-
-app.get('/api/posts', (req, res, next) => {
-    Post.find().then(documets => {
-        res.status(200).json({
-            message: 'Posts fetched successfully!',
-            posts: documets
-        });
-    });
-});
-
-app.get('/api/posts/:id', (req, res, next) => {
-    Post.findById(req.params.id).then(post => {
-        if (post) {
-            res.status(200).json(post);
-        } else {
-            res.status(404).json({ message: 'Post not found!' });
-        }
-    })
-});
-
-
-// Mia prova:
-// app.get('/api/posts/:id', (req, res, next) => {
-//     Post.findOne({
-//         _id: req.params.id
-//     }).then(document => {
-//         res.status(200).json({
-//             doc: document
-//         });
-//     });
-// });
-
-app.delete("/api/posts/:id", (req, res, next) => {
-    // console.log(req.params.id);
-    Post.deleteOne({ _id: req.params.id }).then(result => {
-        console.log(result);
-        res.status(200).json({ message: 'Post deleted!' });
-    });
-});
+app.use(postRoutes)
 
 module.exports = app;
---
+++ backend/routes/posts.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const Post = require('../models/post');
+
+const router = express.Router();
+
+router.post("/api/posts", (req, res, next) => {
+    const post = new Post({
+        _id: req.body.id,
+        title: req.body.title,
+        content: req.body.content
+    });
+    post.save().then(createdPost => {
+        res.status(201).json({
+            message: 'Post added successfully',
+            postId: createdPost._id
+        });
+    });
+});
+
+router.put("/api/posts/:id", (req, res, next) => {
+    const post = new Post({
+        _id: req.body.id,
+        title: req.body.title,
+        content: req.body.content
+    });
+    Post.updateOne({ _id: req.params.id }, post).then(result => {
+        // console.log(result);
+        res.status(200).json({ message: 'Update successful!' });
+    });
+});
+
+router.get('/api/posts', (req, res, next) => {
+    Post.find().then(documets => {
+        res.status(200).json({
+            message: 'Posts fetched successfully!',
+            posts: documets
+        });
+    });
+});
+
+router.get('/api/posts/:id', (req, res, next) => {
+    Post.findById(req.params.id).then(post => {
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: 'Post not found!' });
+        }
+    })
+});
+
+
+// Mia prova:
+// router.get('/api/posts/:id', (req, res, next) => {
+//     Post.findOne({
+//         _id: req.params.id
+//     }).then(document => {
+//         res.status(200).json({
+//             doc: document
+//         });
+//     });
+// });
+
+router.delete("/api/posts/:id", (req, res, next) => {
+    // console.log(req.params.id);
+    Post.deleteOne({ _id: req.params.id }).then(result => {
+        console.log(result);
+        res.status(200).json({ message: 'Post deleted!' });
+    });
+});
+
+module.exports = router;

707fff8af015edc735543c5cdeb5bd31eb97d0c2
zioIgna <ignaziocarbonaro@hotmail.com>
2018-07-01 19:08:15 +0200

    6.77 Uploading files (funzionante, anche preview)

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,76 +1,82 @@
 const express = require("express");
-const Post = require('../models/post');
-const multer = require('multer');
+const Post = require("../models/post");
+const multer = require("multer");
 
 const router = express.Router();
 
 const MIME_TYPE_MAP = {
-    'image/png': 'png',
-    'image/jpeg': 'jpg',
-    'image/jpg': 'jpg'
+  "image/png": "png",
+  "image/jpeg": "jpg",
+  "image/jpg": "jpg"
 };
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const isValid = MIME_TYPE_MAP[file.mimetype];
-        let error = new Error('Invalid mime type');
-        if (isValid) {
-            error = null;
-        }
-        cb(error, 'backend/images');
-    },
-    filename: (req, file, cb) => {
-        const name = file.originalname.toLowerCase().split(' ').join('-');
-        const ext = MIME_TYPE_MAP[file.mimetype];
-        cb(null, name + '-' + Date.now() + '.' + ext);
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid mime type");
+    if (isValid) {
+      error = null;
     }
+    cb(error, "backend/images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname
+      .toLowerCase()
+      .split(" ")
+      .join("-");
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + "-" + Date.now() + "." + ext);
+  }
 });
 
-router.post("", multer({storage: storage}).single('image'), (req, res, next) => {
+router.post(
+  "",
+  multer({ storage: storage }).single("image"),
+  (req, res, next) => {
     const post = new Post({
-        // _id: req.body.id,        //questa linea non deve esserci! altrim si forza il DB a dare _id = null al nuovo post
-        title: req.body.title,
-        content: req.body.content
+      // _id: req.body.id,        //questa linea non deve esserci! altrim si forza il DB a dare _id = null al nuovo post
+      title: req.body.title,
+      content: req.body.content
     });
     post.save().then(createdPost => {
-        res.status(201).json({
-            message: 'Post added successfully',
-            postId: createdPost._id
-        });
+      res.status(201).json({
+        message: "Post added successfully",
+        postId: createdPost._id
+      });
     });
-});
+  }
+);
 
 router.put("/:id", (req, res, next) => {
-    const post = new Post({
-        _id: req.body.id,
-        title: req.body.title,
-        content: req.body.content
-    });
-    Post.updateOne({ _id: req.params.id }, post).then(result => {
-        // console.log(result);
-        res.status(200).json({ message: 'Update successful!' });
-    });
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({ _id: req.params.id }, post).then(result => {
+    // console.log(result);
+    res.status(200).json({ message: "Update successful!" });
+  });
 });
 
-router.get('', (req, res, next) => {
-    Post.find().then(documets => {
-        res.status(200).json({
-            message: 'Posts fetched successfully!',
-            posts: documets
-        });
+router.get("", (req, res, next) => {
+  Post.find().then(documets => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documets
     });
+  });
 });
 
-router.get('/:id', (req, res, next) => {
-    Post.findById(req.params.id).then(post => {
-        if (post) {
-            res.status(200).json(post);
-        } else {
-            res.status(404).json({ message: 'Post not found!' });
-        }
-    })
+router.get("/:id", (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "Post not found!" });
+    }
+  });
 });
-
 
 // Mia prova:
 // router.get('/:id', (req, res, next) => {
@@ -84,11 +90,11 @@ router.get('/:id', (req, res, next) => {
 // });
 
 router.delete("/:id", (req, res, next) => {
-    // console.log(req.params.id);
-    Post.deleteOne({ _id: req.params.id }).then(result => {
-        console.log(result);
-        res.status(200).json({ message: 'Post deleted!' });
-    });
+  // console.log(req.params.id);
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Post deleted!" });
+  });
 });
 
 module.exports = router;
